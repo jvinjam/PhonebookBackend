0143b667d8a61f5df40019ca7ca9a916a8fc7f83
jvinjam <jvinjam@gmail.com>
2025-02-15 23:32:13 -0500

    part3 exercises - 3.12: Command-line database Create and query a MongoDB database using the command line.

---
+++ mongo.js
@@ -0,0 +1,47 @@
+// mongodb+srv://jvinj:<db_password>@cluster0.3pcix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// const url =
+//   `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+const mongoose = require("mongoose");
+
+const numOfArgs = process.argv.length;
+
+if (numOfArgs < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://jvinj:${password}@cluster0.3pcix.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (numOfArgs === 3) {
+  Person.find({}).then((result) => {
+    console.log("Phonebook:");
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+} else if (numOfArgs === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
